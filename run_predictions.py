d719f4a42d4239847d642fd899f2e290ec8a7380
preste-annav <anastasiia.navalikhina@preste.ai>
2021-03-16 08:36:09 +0200

    Comments added + parameters.json modified

--- run_predictions.py
+++ run_predictions.py
@@ -9,31 +9,37 @@ if __name__ == "__main__":
 
     freeze_support()
 
-    dataset = pd.read_csv('data/predictions/test.csv')
-    target = 'substrates'
-
+    # read parameters file
     with open('parameters.json', 'r') as read_file:
-        parameters = json.load(read_file)[target]
+        parameters = json.load(read_file)
 
+    # read data with molecules to predict
+    dataset = pd.read_csv(parameters['predictions']['input_data'])
+    target = parameters['predictions']['target']
+
+    # describe molecules
     descriptor = MordredFeaturizer(data=dataset)
     described_data = descriptor.described_molecules.drop('Smiles', axis=1)
 
-    selected_data = described_data.loc[:, parameters['features']]
+    # select necessary descriptors for model
+    selected_data = described_data.loc[:, parameters[target]['features']]
 
-    for model_path in glob.glob(parameters['trained_transformers'] + '/*.pkl'):
+    # transform data with PCA
+    for model_path in glob.glob(parameters[target]['trained_transformers'] + '/*.pkl'):
         with open(model_path, 'rb') as file:
             transformer = pickle.load(file)
             transformed = transformer.model.transform(selected_data)
 
-    for model_path in glob.glob(parameters['trained_models'] + '/*.pkl'):
+    # run predicting models
+    for model_path in glob.glob(parameters[target]['trained_models'] + '/*.pkl'):
         with open(model_path, 'rb') as file:
-
             model = pickle.load(file)
             model_name = type(model).__name__
 
+            # write predictions to file
             if 'Log' in model_name:
                 predictions = pd.concat([dataset, pd.DataFrame(model.predict(transformed))], axis=1)
-                predictions.to_csv(f'data/predictions/predictions{model_name}.csv')
+                predictions.to_csv(parameters['predictions']['output_data'].format(model_name))
             else:
                 predictions = pd.concat([dataset, pd.DataFrame(model.predict(selected_data))], axis=1)
-                predictions.to_csv(f'data/predictions/predictions{model_name}.csv')
+                predictions.to_csv(parameters['predictions']['output_data'].format(model_name))
